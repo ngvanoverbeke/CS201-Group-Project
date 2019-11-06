4f5b6b7fc22b101357d8260c463e97dbd545a5ba
Andrew Kozak <Canooka143@hotmail.com>
2019-11-06 09:44:37 -0900

    Laying ground work for prototype of the code

--- Project/Main.hpp
+++ Project/Main.hpp
@@ -1,1 +1,27 @@
 //This hpp file wil be used to link everything
+
+#ifndef FILE_MAIN_HPP
+#define FILE_MAIN_HPP
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <string>
+using std::string;
+using std::getline;
+#include <vector>
+using std::vector;
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- Project/main.cpp
+++ Project/main.cpp
@@ -1,1 +1,39 @@
-//This is where everything is referenced and will include the vidual part of the project
+//This is where everything is referenced and will include the visual part of the project
+
+#include "Main.hpp"
+
+int main()
+{
+	string password;
+	string address;
+	int x = 1;
+
+	while (x > 0)
+	{
+		cout << "Hello would you like to store a password(1), Look for a password(2), or exit(0)";
+		cin >> x;
+
+		if (x == 0)
+		{
+			return 0;
+		}
+		else if (x == 1)
+		{
+			cout << "Enter a password:";
+			getline(cin, password);
+			cout << "Enter an address:";
+			getline(cin, address);
+			//storepassword(address, password);
+		}
+		else if (x == 2)
+		{
+			cout << "What address do you want a password for:";
+			getline(cin, address);
+			//searchpassword(address);
+		}
+		else
+		{
+			cout << "Nonvalid number" << endl;
+		}
+	}
+}
